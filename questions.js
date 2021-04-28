aef405f70da0bc58787eb19bf69c0447ee589b41
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-28 12:45:05 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"If you don't know any words, look them ___ in a dictionary.",
+        options:["up", "through", "out", "on"],
+        answer: 0
+    },
+    {
+     q:"Can you look ___ my cat when I'm away.",
+        options:["through", "after", "up", "for"],
+        answer: 1   
+    },
+    {
+        q:"I woke up earlier than usual to look ___ my notes.",
+        options:["out", "up", "through", "back"],
+        answer: 2
+    },
+    {
+        q:"Look ___ ! I've just mopped the floor and it's wet. ",
+        options:["back", "in", "on", "out"],
+        answer: 3
+    }
+]
